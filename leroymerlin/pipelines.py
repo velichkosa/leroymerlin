c245a097bfc28eb2a4b447879f8d78c455f781af
velichkosa <velichko.sergey.a@gmail.com>
2021-08-22 22:28:49 +0500

    final

--- leroymerlin/pipelines.py
+++ leroymerlin/pipelines.py
@@ -2,7 +2,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+#
 
 # useful for handling different item types with a single interface
 
--- leroymerlin/runner.py
+++ leroymerlin/runner.py
@@ -13,3 +13,4 @@ if __name__ == '__main__':
     process.crawl(LeroySpider)
 
     process.start()
+#
